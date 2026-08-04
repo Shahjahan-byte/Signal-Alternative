1bf865c970eccf082b8673fc07b39857050236e2
Shahjahan-byte <sshahjahand@gmail.com>
2026-08-04 15:23:36 +0500

    changes updated

--- react-firebase-chat/src/components/list/chatlist/chatlist.jsx
+++ react-firebase-chat/src/components/list/chatlist/chatlist.jsx
@@ -3,8 +3,8 @@ import "./chatlist.css"
 const Chatlist = () => {
   return (
     <div className='chatlist'>
-      <div className="search">
-        <div className="searchbar">
+      <div className="Search">
+        <div className="Searchbar">
           <img src="./search.png" alt="" />
           <input type="text" placeholder="Search" />
         </div>

9b462f6c7460496b103892b3a2f3dd2879a438db
sshahjahand@gmail.com <sshahjahand@gmail.com>
2026-08-05 22:35:04 +0500

    Implement emoji handling and state management in chat component

--- react-firebase-chat/src/components/chat/chat.jsx
+++ react-firebase-chat/src/components/chat/chat.jsx
@@ -3,7 +3,13 @@ import "./chat.css"
 import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
-const [open,setOpen] = useState(false)
+    const [open,setOpen] = useState(false)
+    const [text,setText] = useState("")
+
+    const handleEmoji = e => {
+        setText((prev) => prev + e.emoji)
+        setOpen(false)
+    };
 
     return (
         <div className='chat'>
@@ -28,10 +34,13 @@ const [open,setOpen] = useState(false)
                     <img src="./camera.png" alt="" />
                     <img src="./mic.png" alt="" />
                 </div>
-                <input type="text" placeholder="Type a message..." />
+                <input type="text" placeholder="Type a message..."
+                value={text}
+                onChange={e=>setText(e.target.value)} />
                 <div className="emoji">
-                    <img src="./emoji.png" alt=""onClick={()=>setOpen(prev=> !prev)} />
-                    <EmojiPicker open={open}/>
+                    <img src="./emoji.png" alt=""onClick={()=>setOpen(prev=> !prev)} 
+                    />
+                    <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
                 </div>
                 <button className="sendButton">Send</button>
             </div>

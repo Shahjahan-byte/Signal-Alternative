b871e027101e66021cda1a6de09110f1093fe09c
sshahjahand@gmail.com <sshahjahand@gmail.com>
2026-08-09 16:59:04 +0500

    Add user detail component with avatar and chat settings

--- react-firebase-chat/src/components/details/detail.jsx
+++ react-firebase-chat/src/components/details/detail.jsx
@@ -2,7 +2,40 @@ import "./detail.css"
 
 const Detail = () => {
   return (
-    <div className='detail'>Detail</div>
+    <div className='detail'>
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Shahjahan</h2>
+        <p>Hello Meet The Creater of Linux.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Setting</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+          <div className="title">
+            <span>Chat Setting</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+          <div className="title">
+            <span>Privacy % help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+          <div className="title">
+            <span>Shared photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%2Fid%2FOIP.YQOk_z5zu9r1pPj5rBzq7QHaHa%3Fpid%3DApi&f=1&ipt=4db808904ccb84e59140fb916f17431845eb86f42427e32455bd98460597f9b8&ipo=images" alt="" />
+              <span>photo_2026_2.png</span>
+            </div>
+            <img src="./download.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 

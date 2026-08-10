592e1b45983195c2ba399973d51f5fd383d0a04c
sshahjahand@gmail.com <sshahjahand@gmail.com>
2026-08-10 23:08:27 +0500

    Implement Login component with form structure and styling

--- react-firebase-chat/src/components/login/Login.jsx
+++ react-firebase-chat/src/components/login/Login.jsx
@@ -2,7 +2,25 @@ import "./login.css";
 
 const Login = () => {
   return (
-    <div className='login'>Login</div>
+    <div className='login'>
+        <div className="items">
+            <h2>Welcome Back,</h2>
+            <form>
+                <input type="text" placeholder="Email" name="email" />
+                <input type="password" placeholder="password" name="password" />
+                <button>Sign In</button>
+            </form>
+        </div>
+        <div className="separator"></div>
+        <div className="items">
+              <h2>Create an Account</h2>
+            <form>
+                <input type="text" placeholder="Email" name="email" />
+                <input type="password" placeholder="password" name="password" />
+                <button>Sign In</button>
+            </form>
+        </div>
+    </div>
   )
 }
 

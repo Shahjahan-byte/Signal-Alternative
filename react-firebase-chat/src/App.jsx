2a03c2f1db3d9ea460e1c763fc1cba2a6569bda7
Shahjahan-byte <sshahjahand@gmail.com>
2026-08-11 16:13:48 +0500

    Updated

--- react-firebase-chat/src/App.jsx
+++ react-firebase-chat/src/App.jsx
@@ -2,7 +2,7 @@ import List from './components/list/list'
 import Chat from './components/chat/chat'
 import Detail from './components/details/detail'
 import Login from './components/login/Login';
-
+import Notification from './components/notification/Notification'; // Add this line!
 
 const App = () => {
  

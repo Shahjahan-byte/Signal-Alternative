e23f7b67171296e560dfd26b5a4a0734d93d1704
sshahjahand@gmail.com <sshahjahand@gmail.com>
2026-08-11 09:19:45 +0500

    Add file upload input and username field to account creation form

--- react-firebase-chat/src/components/login/Login.jsx
+++ react-firebase-chat/src/components/login/Login.jsx
@@ -15,6 +15,9 @@ const Login = () => {
         <div className="items">
               <h2>Create an Account</h2>
             <form>
+                <label htmlFor="file">Upload an image</label>
+                <input type="file" id="file" style={{display:"none"}}/>
+                <input type="text" placeholder="Username" name="username" />
                 <input type="text" placeholder="Email" name="email" />
                 <input type="password" placeholder="password" name="password" />
                 <button>Sign In</button>

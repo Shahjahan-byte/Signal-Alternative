0af56f4b29023bf01cf25dd39db04f8681f95487
Shahjahan-byte <sshahjahand@gmail.com>
2026-08-11 16:56:13 +0500

    Implement AddUser component with form and user display

--- react-firebase-chat/src/components/list/chatlist/chatlist.jsx
+++ react-firebase-chat/src/components/list/chatlist/chatlist.jsx
@@ -1,5 +1,6 @@
 import  { useState } from "react"
 import "./chatlist.css"
+import AddUser from "./addUser";
 
 const Chatlist = () => {
   const [addMode, setAddMode]= useState(false)
@@ -42,6 +43,7 @@ const Chatlist = () => {
         <p>Salam</p>
        </div>
       </div>
+      <AddUser/>
     </div>
   );
 };

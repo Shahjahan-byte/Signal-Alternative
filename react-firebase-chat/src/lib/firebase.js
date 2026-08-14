6751ab1756be99af413886c329bae149a798b2ef
Shahzaman Durrani <shahzamandurrani01@gmail.com>
2026-08-14 20:33:31 +0500

    afixed auth

--- react-firebase-chat/src/lib/firebase.js
+++ react-firebase-chat/src/lib/firebase.js
@@ -1,18 +1,16 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import {getStorage} from "firebase/storage";
+  import { initializeApp } from "firebase/app";
+  import { getAuth } from "firebase/auth";
+  import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "reactchat-b3739.firebaseapp.com",
-  projectId: "reactchat-b3739",
-  storageBucket: "reactchat-b3739.firebasestorage.app",
-  messagingSenderId: "332330780308",
-  appId: "1:332330780308:web:5c021c4a17daa2d33c5681"
-};
+  const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: "reactchat-b3739.firebaseapp.com",
+    projectId: "reactchat-b3739",
+    storageBucket: "reactchat-b3739.firebasestorage.app",
+    messagingSenderId: "332330780308",
+    appId: "1:332330780308:web:5c021c4a17daa2d33c5681"
+  };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth();
-export const db = getFirestore();
-export const storage = getStorage();
+  const app = initializeApp(firebaseConfig);
+  export const auth = getAuth();
+  export const db = getFirestore();
